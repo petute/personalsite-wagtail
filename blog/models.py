d235f1815c1ef92bd983ba8ed8fed591a6f21c1b
petute <daniel.petutschnigg@gmail.com>
2021-02-05 08:43:13 +0100

    Add simple blog Added a simple blog model and the correlating templates.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.core.models import Page
+from wagtail.images.blocks import ImageChooserBlock
 
 # Create your models here.
+
+class _S_HeaderBlock(blocks.StructBlock):
+    pass
+
+
+class _S_BlogPostBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    post = blocks.RichTextBlock()
+
+    class Meta:
+        template = 'blog/blocks/_S_BlogPost.html'
+
+class _S_BlogBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    posts = blocks.StreamBlock([
+        ('post', _S_BlogPostBlock())
+    ],null=True)
+
+    class Meta:
+        template = 'blog/blocks/_S_Blog.html'
+
+class _S_FooterBlock(blocks.StructBlock):
+    pass
+
+class BlogPage(Page):
+    sections = StreamField([
+        ('Blog', _S_BlogBlock()),
+    ], null=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('sections'),
+    ]
+
+    template = 'blog/blog_page.html'
